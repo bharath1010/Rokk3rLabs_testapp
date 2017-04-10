d11696a38ef0951834df797dd18744abc717bd06
smkalaivanan <kalaivanan@falconnect.in>
2017-04-10 20:49:17 +0530

    Initial Commit

---
+++ Rokk3rSampleApplication/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Rokk3rSampleApplication
+//
+//  Created by apple on 4/10/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Rokk3rSampleApplication/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Rokk3rSampleApplication
+//
+//  Created by apple on 4/10/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
